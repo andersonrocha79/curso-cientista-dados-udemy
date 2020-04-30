7635db012523ce3507652ece5b22bdd56c6ecab2
Anderson Rocha <anderson_rocha79@hotmail.com>
2020-04-30 07:45:36 -0300

    aulas de python e estatistica i

---
+++ Python/2.2-variaveis e objetos.py
@@ -0,0 +1,35 @@
+x = 10
+y = 20
+z = 100
+w = (x + y) * z / 100
+
+print(w)
+print("este texto será impresso no console")
+print(x)
+print("texto e duas variáveis", x, ", ", z)
+
+m = "python"
+print("tipo da variável m: ", type(m))
+print("tipo da variável x: ", type(x))
+
+print("informe o valor 1:")
+i = input()
+print(type(i))
+
+var = input("informe o valor 2:")
+var = int(var)
+print(type(var))
+var = float(var)
+print(type(var))
+
+# teste de comentário
+
+'''
+comentário em várias linhas
+teste1
+teste2
+teste3
+teste4
+'''
+
+print("fim dos comentários")
---
+++ Python/3.2-estrutura de decisao.py
@@ -0,0 +1,23 @@
+print("exemplo 1")
+
+nota = 7
+
+if nota >= 7:
+    print("aprovado")
+    print("parabens")
+else:
+    print("reprovado")
+    print("teste novamente")
+    
+
+print("exemplo 2")
+
+nota = 5
+
+if nota <= 4:
+    print("reprovado")
+    print("reprovado")    
+elif nota > 4 and nota <= 6:
+    print("exame")
+else:
+    print("aprovado")
